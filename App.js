38abf4206bed9d143e6ac4642eb793668a87e032
Alex <alexander.salvatore@gmail.com>
2020-05-28 14:39:56 +0200

    finalization du projet

--- App.js
+++ App.js
@@ -1,19 +1,164 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { Platform } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  TextInput,
+  Alert,
+  Button,
+  FlatList,
+} from "react-native";
+import * as firebase from "firebase";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+// Initialize Firebase
+const firebaseConfig = {
+  /* YOUR FIRE BASE CONFIG GOES HERE */
+};
+
+if (!firebase.apps.length > 0) firebase.initializeApp(firebaseConfig);
+
+export default class App extends Component {
+  state = {
+    name: "alex",
+    message: "Some text",
+    messages: [],
+  };
+
+  onChangeText(field_) {
+    return (val_) => {
+      this.setState({
+        [field_]: val_,
+      });
+    };
+  }
+
+  onSend = () => {
+    firebase
+      .database()
+      .ref("messages")
+      .push({
+        date: Date.now(),
+        message: this.state.message,
+        name: this.state.name,
+      })
+      .then(() => {
+        Alert.alert("message send");
+      })
+      .catch((err_) => {
+        console.error(err_);
+      });
+  };
+
+  componentDidMount() {
+    firebase
+      .database()
+      .ref("messages")
+      .on("child_added", (snapshot) => {
+        let messagesNew = this.state.messages;
+        messagesNew.push(snapshot.val());
+        //Sort the array by datefrom most recent
+        messagesNew.sort((a, b) => {
+          return a > b;
+        });
+        this.setState({
+          messages: messagesNew,
+        });
+      });
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        {Platform.OS === "android" && <View style={styles.statusBar}></View>}
+        <FlatList
+          style={styles.containerChat}
+          data={this.state.messages}
+          renderItem={({ item }) => {
+            return (
+              <View style={styles.chatItem}>
+                <Text style={styles.chatName}>
+                  {"@" +
+                    item.name +
+                    " at " +
+                    new Date(item.date).toLocaleTimeString()}
+                </Text>
+                <Text style={styles.chatMessage}>{item.message}</Text>
+              </View>
+            );
+          }}
+          keyExtractor={(item) => item.date}
+        ></FlatList>
+        {/*this.state.messages.map((item_) => (
+            <Text key={item_.Date}>{item_.message}</Text>
+          ))*/}
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            value={this.state.name}
+            onChangeText={this.onChangeText("name")}
+          ></TextInput>
+          <TextInput
+            style={styles.input}
+            value={this.state.message}
+            onChangeText={this.onChangeText("message")}
+          ></TextInput>
+          <Button title="Send" onPress={this.onSend}></Button>
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fee2b3",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  statusBar: {
+    backgroundColor: "#562349",
+    width: "100%",
+    height: 28,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    margin: 14,
+  },
+
+  inputContainer: {
+    width: "100%",
+  },
+
+  input: {
+    backgroundColor: "#fff",
+    padding: 4,
+    margin: 4,
+    borderRadius: 4,
+  },
+
+  chatItem: {
+    backgroundColor: "#562349",
+    padding: 8,
+    margin: 8,
+    borderRadius: 4,
+  },
+
+  containerChat: {
+    width: "100%",
+  },
+
+  chatName: {
+    color: "#ad6989",
+    fontSize: 20,
+  },
+
+  chatMessage: {
+    color: "#ffa299",
+    fontSize: 20,
   },
 });
---
+++ README.md
@@ -0,0 +1,23 @@
+# Live Coding ⌨️ @Hardis-Group
+
+Session de livecoding découverte de React Native Expo & Firebase Database, avec le développement d'une app de chat.
+
+![App de chat React Native](https://pbs.twimg.com/media/EZGraQ5WAAMaw0X?format=jpg&name=medium)
+
+## Installation
+
+Générer un projet expo: [https://docs.expo.io/](https://docs.expo.io/)
+Pour rappel, Expo est un Framework sur le top de React Native qui permet de faire tourner un projet React Native sur n'importe qu'elle device ou émulateur. Ce framework a des limitations en terme de fonctionnalités, qui énumérées [ici](https://docs.expo.io/introduction/why-not-expo/). Pour créer un projet React Native sans Expo, vous trouverez les infos [ici](https://reactnative.dev/docs/environment-setup), dans l'onglet "React Native CLI QuickStart".
+
+## Application Expo
+
+Pour voir en live votre application sur votre téléphone portable scannez le QR code de votre interface Expo avec l'[app Expo](https://play.google.com/store/apps/details?id=host.exp.exponent&hl=en). A noter que vous devez être sur le même réseau Wifi que votre ordinateur, ou sinon sélectionner l'option "tunnel".
+
+## Intégration de Firebase
+
+La meilleure documentation pour intégrer Firebase se trouve [dans la doc Expo](https://docs.expo.io/guides/using-firebase/).
+Vous pouvez créer gratuitement un projet Firebase sur [la console Firebase](https://console.firebase.google.com/). Pensez à bien selectionner une base de données Realtime Database, et à bien mettre toutes les "rules", règles d'accès aux données à true.
+
+## Pour approfondir
+
+React Native & ReactJS font appel aux concepts de programmation fonctionnel. Pour voir ce type de programmation + en détails je vous recommande (ce cours, en anglais)[https://youtu.be/poQXNp9ItL4?t=934].
